95e18f9e7098ca1dc9d7eb04b2ed3f7324be7973
pow2267 <pow2267@gmail.com>
2021-06-10 14:47:39 +0900

    [Programmers] upgrade codes

--- ClawMachine/main.swift
+++ ClawMachine/main.swift
@@ -8,33 +8,22 @@
 import Foundation
 
 func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
-    var columns = [[Int]](repeating: [Int](), count: board.count)
-    
-    for b in board { // 각 행을 열 별로 재정리
-        for (j, v) in b.enumerated() {
-            if v > 0 {
-                columns[j].append(v)
-            }
-        }
-    }
-    
+    var vBoard = board
     var moved = [Int]()
     var count = 0
     
-    for move in moves {
-        if columns[move-1].count > 0 { // 집게가 집은 열에 무언가 존재할 때
-            if moved.count == 0 { // 집게가 옮긴 통이 비었을 때
-                moved.append(columns[move-1].first!)
-            } else {
-                if columns[move-1].first == moved.last {
-                    count += 2 // 집게가 집은 인형과 통의 맨 위에 있는 인형 합쳐 두 개
-                    moved.removeLast() // 통에 있는 인형 사라짐
+    moves.forEach { move in
+        for (i, b) in vBoard.enumerated() {
+            if b[move-1] > 0 {
+                if (moved.count > 0 ? moved.last : 0) == b[move-1]{
+                    count += 2
+                    moved.removeLast()
                 } else {
-                    moved.append(columns[move-1].first!) // 인형을 통에 쌓음
-                    
+                    moved.append(b[move-1])
                 }
+                vBoard[i][move-1] = 0
+                break
             }
-            columns[move-1].removeFirst() // 집게로 집은 인형 사라짐
         }
     }
     
