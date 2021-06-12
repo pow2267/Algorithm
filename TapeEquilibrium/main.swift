ee6820a9c5b4d434ea9c1cf9a3a38142705a0211
pow2267 <pow2267@gmail.com>
2021-06-13 01:58:01 +0900

    [Codility] Remove useless tabs

--- TapeEquilibrium/main.swift
+++ TapeEquilibrium/main.swift
@@ -9,24 +9,24 @@ import Foundation
 
 func solution(_ A : inout [Int]) -> Int {
     var leftSum = 0
-        var rightSum = 0
-        A.forEach {
-            rightSum += $0
+    var rightSum = 0
+    A.forEach {
+        rightSum += $0
+    }
+    var difference = 0
+    var result = 0
+
+    for i in 1..<A.count {
+        leftSum += A[i-1]
+        rightSum -= A[i-1]
+        difference = abs(leftSum-rightSum)
+
+        if i == 1 {
+            result = difference
+        } else {
+            result = (result > difference ? difference : result)
         }
-        var difference = 0
-        var result = 0
+    }
 
-        for i in 1..<A.count {
-            leftSum += A[i-1]
-            rightSum -= A[i-1]
-            difference = abs(leftSum-rightSum)
-
-            if i == 1 {
-                result = difference
-            } else {
-                result = (result > difference ? difference : result)
-            }
-        }
-
-        return result
+    return result
 }
