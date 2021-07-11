f171a2776cdbf9dd1e7b0460cc0ab31932c17352
pow2267 <pow2267@gmail.com>
2021-07-11 15:49:45 +0900

    [Programmers] Upgrade codes

--- LargestNumber/main.swift
+++ LargestNumber/main.swift
@@ -8,40 +8,17 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    var nums = [[String]]()
-    var result = ""
+    // Int -> String으로 변환
+    var nums = numbers.map { String($0) }
     
-    for number in numbers {
-        nums.append(String(number).map{String($0)})
+    // 두 숫자를 합쳐서 더 큰 숫자가 되는 걸 앞에 배치
+    nums.sort { $0+$1 > $1+$0 }
+    
+    // [0, 0, 0]경우 "000"으로 표기되므로 "0"으로 반환
+    if nums[0] == "0" {
+        return "0"
     }
     
-    while !nums.isEmpty {
-        var first = 0 // 가장 큰 첫번째 숫자
-        var largest = [[String]]()
-        
-        for num in nums {
-            first = (Int(num[0])! > first ? Int(num[0])! : first)
-        }
-        
-        for (index, num) in nums.enumerated() {
-            if Int(num[0])! == first {
-                largest.append(num)
-                nums[index] = ["-1"]
-            }
-        }
-        nums = nums.filter { $0 != ["-1"] }
-        
-        if largest.count == 1 {
-            for value in largest[0] {
-                result += value
-            }
-            continue
-        }
-        
-        // 첫번째 수가 같은 숫자들 순서 정하는 로직
-        
-        print(largest)
-    }
-    
-    return result
+    // 배열을 풀어서 하나의 String으로 반환
+    return nums.joined()
 }
