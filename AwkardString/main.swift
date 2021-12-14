b1ed2031929cdbac570c6a0e1cdb38e1719549f6
pow2267 <pow2267@gmail.com>
2021-12-14 23:12:19 +0900

    [Programmers] Change reduce to joined

--- AwkardString/main.swift
+++ AwkardString/main.swift
@@ -15,34 +15,22 @@ func solution(_ s: String) -> String {
     // 또한, 이 문제에서는 여분의 공백이 있는지 여부가 중요한데 split은 여분의 공백을 없애버려서
     // split 대신 components를 사용했다. (components는 바로 String을 반환해준다.)
     var strs: [String] = s.components(separatedBy: " ")
-    var result = ""
     
     for (idx, str) in strs.enumerated() {
         var letters: [String] = str.map({String($0)})
         
-        if str != " " {
-            for (index, letter) in letters.enumerated() {
-                if index % 2 == 1 {
-                    // isLowercase는 Character에서 가능하지만
-                    // lowercased()는 String에서 가능하다.
-                    letters[index] = letter.lowercased()
-                } else {
-                    letters[index] = letter.uppercased()
-                }
-            }
-
-            strs[idx] = letters.reduce("", {(_ first: String, _ second: String) in
-                        return first + second })
-
-            if idx == 0 {
-                result += strs[idx]
+        for (index, letter) in letters.enumerated() {
+            if index % 2 == 1 {
+                // isLowercase는 Character에서 가능하지만
+                // lowercased()는 String에서 가능하다.
+                letters[index] = letter.lowercased()
             } else {
-                result += (" " + strs[idx])
+                letters[index] = letter.uppercased()
             }
-        } else {
-            result += " "
         }
+
+        strs[idx] = letters.joined(separator: "")
     }
     
-    return result
+    return strs.joined(separator: " ")
 }
